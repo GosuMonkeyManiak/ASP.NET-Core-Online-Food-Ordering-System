3bff22005074f2b63a9e3925c757f4a3d80f1bcf
Dimitar Kupanov <dimitarkupanov@gmail.com>
2022-03-07 22:12:12 +0200

    Separate UI for diffrent roles in application

--- FoodFun.Web/Program.cs
+++ FoodFun.Web/Program.cs
@@ -62,6 +62,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "Administration",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 

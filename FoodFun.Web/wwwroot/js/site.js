c33f4bff0f2e68be84c76b950268323dcfb313d3
Dimitar Kupanov <dimitarkupanov@gmail.com>
2022-03-18 09:29:52 +0200

    Added automatically to see inputed image for product or dish also when editing

--- FoodFun.Web/wwwroot/js/site.js
+++ FoodFun.Web/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$('#imageInput').focusout(() => {
+    $('#image').attr('src', $('#imageInput').val());
+});
 
-// Write your JavaScript code.
+$(document).ready(() => {
+    $('#image').attr('src', $('#imageInput').val());
+});

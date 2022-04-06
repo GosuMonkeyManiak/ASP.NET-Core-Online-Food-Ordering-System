420e6255f12495911ee0ac9fda20da64acacd654
Dimitar Kupanov <dimitarkupanov@gmail.com>
2022-04-06 16:25:58 +0300

    Extract order controller in root application

--- FoodFun.Web/Areas/Order/Controllers/OrderController.cs
+++ FoodFun.Web/Areas/Order/Controllers/OrderController.cs
@@ -1,53 +1,12 @@
 ﻿namespace FoodFun.Web.Areas.Order.Controllers
 {
-    using Core.Contracts;
-    using FoodFun.Core.Extensions;
-    using FoodFun.Core.Models.Cart;
-    using global::AutoMapper;
-    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
-    using Models.Cart;
-    using System.Security.Claims;
 
     using static Constants.GlobalConstants.Areas;
-    using static Constants.GlobalConstants.Messages;
 
     [Area(Order)]
     public class OrderController : Controller
     {
-        private readonly IOrderService orderService;
-        private readonly IMapper mapper;
-
-        public OrderController(
-            IOrderService orderService,
-            IMapper mapper)
-        {
-            this.orderService = orderService;
-            this.mapper = mapper;
-        }
-
-        [HttpPost]
-        [Authorize]
-        public async Task<IActionResult> Create(CartListingModel cartItems)
-        {
-            if (!this.ModelState.IsValid)
-            {
-                this.TempData[Error] = SomethingWentWrong;
-
-                return RedirectToAction("Cart", "Cart", new { area = "" });
-            }
-
-            var userId = this.User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value;
-
-            var orderNumber = await this.orderService
-                .Create(
-                    userId, 
-                    cartItems.Products.ProjectTo<CartItemModel>(this.mapper),
-                    cartItems.Dishes.ProjectTo<CartItemModel>(this.mapper));
-
-            this.HttpContext.Session.Clear();
-
-            return View(orderNumber);
-        }
+        
     }
 }
---
+++ FoodFun.Web/Controllers/OrderController.cs
@@ -0,0 +1,51 @@
+﻿namespace FoodFun.Web.Controllers
+{
+    using FoodFun.Core.Contracts;
+    using FoodFun.Core.Extensions;
+    using FoodFun.Core.Models.Cart;
+    using FoodFun.Web.Models.Cart;
+    using global::AutoMapper;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Security.Claims;
+
+    using static Constants.GlobalConstants.Messages;
+
+    public class OrderController : Controller
+    {
+        private readonly IOrderService orderService;
+        private readonly IMapper mapper;
+
+        public OrderController(
+            IOrderService orderService,
+            IMapper mapper)
+        {
+            this.orderService = orderService;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create(CartListingModel cartItems)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                this.TempData[Error] = SomethingWentWrong;
+
+                return RedirectToAction("Cart", "Cart", new { area = "" });
+            }
+
+            var userId = this.User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value;
+
+            var orderNumber = await this.orderService
+                .Create(
+                    userId,
+                    cartItems.Products.ProjectTo<CartItemModel>(this.mapper),
+                    cartItems.Dishes.ProjectTo<CartItemModel>(mapper));
+
+            this.HttpContext.Session.Clear();
+
+            return View(orderNumber);
+        }
+    }
+}

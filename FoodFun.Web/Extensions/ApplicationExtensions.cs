1fc41bf7ac532b34655e92ebed26609badf6cbb6
Dimitar Kupanov <dimitarkupanov@gmail.com>
2022-04-15 13:47:21 +0300

    Introduce seeding from json files

--- FoodFun.Web/Extensions/ApplicationExtensions.cs
+++ FoodFun.Web/Extensions/ApplicationExtensions.cs
@@ -5,6 +5,7 @@
     using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
     using Newtonsoft.Json;
+
     using static Constants.GlobalConstants.Roles;
 
     public static class ApplicationExtensions
@@ -16,52 +17,60 @@
             var dbContext = scope.ServiceProvider.GetService<FoodFunDbContext>();
             dbContext.Database.Migrate();
 
-            //if (!db.Roles.Any())
-            //{
-            //    SeedRoles(scope.ServiceProvider.GetService<RoleManager<IdentityRole>>())
-            //        .GetAwaiter()
-            //        .GetResult();
-            //}
+            if (!dbContext.Roles.Any())
+            {
+                SeedRoles(scope.ServiceProvider.GetService<RoleManager<IdentityRole>>())
+                    .GetAwaiter()
+                    .GetResult();
+            }
 
-            //if (!db.Users.Any())
-            //{
-            //    var configuration = scope.ServiceProvider.GetService<IConfiguration>();
+            if (!dbContext.Users.Any())
+            {
+                var configuration = scope.ServiceProvider.GetService<IConfiguration>();
 
-            //    SeedUsers(
-            //        scope.ServiceProvider.GetService<UserManager<User>>(),
-            //        configuration["AdminUser:Username"],
-            //        configuration["AdminUser:Password"],
-            //        configuration["AdminUser:Email"])
-            //        .GetAwaiter()
-            //        .GetResult();
-            //}
+                SeedAdminUser(
+                    scope.ServiceProvider.GetService<UserManager<User>>(),
+                    configuration["AdminUser:Username"],
+                    configuration["AdminUser:Password"],
+                    configuration["AdminUser:Email"])
+                    .GetAwaiter()
+                    .GetResult();
+            }
 
             if (!dbContext.ProductsCategories.Any())
             {
                 SeedCategoriesForProduct(dbContext);
             }
 
-            //if (!db.DishesCategories.Any())
-            //{
-            //    SeedCategoriesForDish(db)
-            //        .GetAwaiter()
-            //        .GetResult();
-            //}
+            if(!dbContext.Products.Any())
+            {
+                SeedProducts(dbContext);
+            }
 
-            //if (!db.TableSizes.Any())
-            //{
-            //    SeedTableSizes(db).GetAwaiter().GetResult();
-            //}
+            if (!dbContext.DishesCategories.Any())
+            {
+                SeedCategoriesForDish(dbContext);
+            }
 
-            //if (!db.TablePositions.Any())
-            //{
-            //    SeedTablePositions(db).GetAwaiter().GetResult();
-            //}
+            if (!dbContext.Dishes.Any())
+            {
+                SeedDishes(dbContext);
+            }
 
-            //if (!db.Tables.Any())
-            //{
-            //    SeedTable(db).GetAwaiter().GetResult();
-            //}
+            if (!dbContext.TableSizes.Any())
+            {
+                SeedTableSizes(dbContext);
+            }
+
+            if (!dbContext.TablePositions.Any())
+            {
+                SeedTablePositions(dbContext);
+            }
+
+            if (!dbContext.Tables.Any())
+            {
+                SeedTable(dbContext);
+            }
         }
 
         private async static Task SeedRoles(RoleManager<IdentityRole> roleManager)
@@ -69,9 +78,10 @@
             await roleManager.CreateAsync(new IdentityRole(Administrator));
             await roleManager.CreateAsync(new IdentityRole(OrderManager));
             await roleManager.CreateAsync(new IdentityRole(RestaurantManager));
+            await roleManager.CreateAsync(new IdentityRole(SupermarketManager));
         }
 
-        private async static Task SeedUsers(
+        private async static Task SeedAdminUser(
             UserManager<User> userManager,
             string userName,
             string password,
@@ -92,7 +102,7 @@
         {
             var productCategoriesSet = File.ReadAllText("DataSets/ProductCategory/productCategories.json");
 
-            List<ProductCategory> categories = JsonConvert.DeserializeObject<List<ProductCategory>>(productCategoriesSet);
+            var categories = JsonConvert.DeserializeObject<List<ProductCategory>>(productCategoriesSet);
 
             dbContext.ProductsCategories
                 .AddRange(categories);
@@ -100,87 +110,76 @@
             dbContext.SaveChanges();
         }
 
-        private async static Task SeedCategoriesForDish(FoodFunDbContext dbContext)
+        private static void SeedProducts(FoodFunDbContext dbContext)
         {
-            List<DishCategory> categories = new()
-            {
-                new() { Title = "Salad" },
-                new() { Title = "Soup" },
-                new() { Title = "Drinks" },
-                new() { Title = "BBQ" },
-                new() { Title = "Pizza" },
-            };
+            var productsSet = File.ReadAllText("DataSets/Product/products.json");
 
-            await dbContext.DishesCategories
-                .AddRangeAsync(categories);
+            var products = JsonConvert.DeserializeObject<List<Product>>(productsSet);
 
-            await dbContext.SaveChangesAsync();
+            dbContext.Products
+                .AddRange(products);
+
+            dbContext.SaveChanges();
         }
 
-        private async static Task SeedTableSizes(FoodFunDbContext dbContext)
+        private static void SeedDishes(FoodFunDbContext dbContext)
         {
-            var tableSizes = new List<TableSize>()
-            {
-                new TableSize() { Seats = 4 },
-                new TableSize() { Seats = 5 },
-                new TableSize() { Seats = 6 },
-                new TableSize() { Seats = 7 },
-                new TableSize() { Seats = 8 },
-                new TableSize() { Seats = 9 },
-                new TableSize() { Seats = 10 },
-            };
+            var dishesSet = File.ReadAllText("DataSets/Dish/dishes.json");
 
-            await dbContext.TableSizes
-                .AddRangeAsync(tableSizes);
+            var dishes = JsonConvert.DeserializeObject<List<Dish>>(dishesSet);
 
-            await dbContext.SaveChangesAsync();
+            dbContext.Dishes
+                .AddRange(dishes);
+
+            dbContext.SaveChanges();
         }
 
-        private async static Task SeedTablePositions(FoodFunDbContext dbContext)
+        private static void SeedCategoriesForDish(FoodFunDbContext dbContext)
         {
-            var tablePositions = new List<TablePosition>()
-            {
-                new TablePosition() { Position = "Out side" },
-                new TablePosition() { Position = "In side" }
-            };
+            var dishCategoriesSet = File.ReadAllText("DataSets/DishCategory/dishCategories.json");
 
-            await dbContext.TablePositions
-                .AddRangeAsync(tablePositions);
+            List<DishCategory> categories = JsonConvert.DeserializeObject<List<DishCategory>>(dishCategoriesSet);
 
-            await dbContext.SaveChangesAsync();
+            dbContext.DishesCategories
+                .AddRange(categories);
+
+            dbContext.SaveChanges();
         }
 
-        private async static Task SeedTable(FoodFunDbContext dbContext)
+        private static void SeedTableSizes(FoodFunDbContext dbContext)
         {
-            var tables = new List<Table>()
-            {
-                new Table() { TableSizeId = 1, TablePositionId = 1},
-                new Table() { TableSizeId = 2, TablePositionId = 1},
-                new Table() { TableSizeId = 3, TablePositionId = 1},
-                new Table() { TableSizeId = 4, TablePositionId = 1},
-                new Table() { TableSizeId = 5, TablePositionId = 1},
-                new Table() { TableSizeId = 6, TablePositionId = 1},
-                new Table() { TableSizeId = 7, TablePositionId = 1},
-                new Table() { TableSizeId = 1, TablePositionId = 1},
-                new Table() { TableSizeId = 2, TablePositionId = 1},
-                new Table() { TableSizeId = 3, TablePositionId = 1},
+            var tableSizesSet = File.ReadAllText("DataSets/TableSize/tableSizes.json");
 
-                new Table() { TableSizeId = 1, TablePositionId = 2},
-                new Table() { TableSizeId = 2, TablePositionId = 2},
-                new Table() { TableSizeId = 3, TablePositionId = 2},
-                new Table() { TableSizeId = 4, TablePositionId = 2},
-                new Table() { TableSizeId = 5, TablePositionId = 2},
-                new Table() { TableSizeId = 6, TablePositionId = 2},
-                new Table() { TableSizeId = 7, TablePositionId = 2},
-                new Table() { TableSizeId = 1, TablePositionId = 2},
-                new Table() { TableSizeId = 2, TablePositionId = 2},
-                new Table() { TableSizeId = 3, TablePositionId = 2},
-            };
+            var tableSizes = JsonConvert.DeserializeObject<List<TableSize>>(tableSizesSet);
 
-            await dbContext.Tables
-                .AddRangeAsync(tables);
+            dbContext.TableSizes
+                .AddRange(tableSizes);
 
-            await dbContext.SaveChangesAsync();
+            dbContext.SaveChanges();
+        }
+
+        private static void SeedTablePositions(FoodFunDbContext dbContext)
+        {
+            var tablePositionsSet = File.ReadAllText("DataSets/TablePosition/tablePositions.json");
+
+            var tablePositions = JsonConvert.DeserializeObject<List<TablePosition>>(tablePositionsSet);
+
+            dbContext.TablePositions
+                .AddRange(tablePositions);
+
+            dbContext.SaveChanges();
+        }
+
+        private static void SeedTable(FoodFunDbContext dbContext)
+        {
+            var tablesSet = File.ReadAllText("DataSets/Table/tables.json");
+
+            var tables = JsonConvert.DeserializeObject<List<Table>>(tablesSet);
+
+            dbContext.Tables
+                .AddRange(tables);
+
+            dbContext.SaveChanges();
         }
     }
 }
